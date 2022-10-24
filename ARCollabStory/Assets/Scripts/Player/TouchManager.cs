e20ac2a4ba3d5a67017366796f41216c881e364c
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-10-24 12:08:46 +0900

    [Refector] 주석 삭제

--- ARCollabStory/Assets/Scripts/Player/TouchManager.cs
+++ ARCollabStory/Assets/Scripts/Player/TouchManager.cs
@@ -96,44 +96,8 @@ public class TouchManager : MonoBehaviour, IInput
 
     public bool OpenBook()
     {
-        //ray = Camera.main.ScreenPointToRay(touch.position);
-        //Debug.Log($"맞은놈 : {hit.transform.position} / 터치위치 : {touch.position}");
-        //hit.transform.position = touch.position;
-
         Debug.Log("스와이프");
 
         return true;
     }
-
-    /*
-    // 생성할 객체
-    private GameObject placeObject;
-    private ARRaycastManager raycastMgr;
-    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-
-    void Start()
-    {
-        // 생성할 큐브 할당
-        placeObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        // 큐브 크기 설정
-        placeObject.transform.localScale = Vector3.one * 0.05f;
-        // AR Raycast Manager 추출
-        raycastMgr = GetComponent<ARRaycastManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.touchCount == 0) return;
-        Touch touch = Input.GetTouch(0);
-        if (touch.phase == TouchPhase.Began)
-        {
-            // 평면으로 인식한 곳만 레이로 검출
-            if (raycastMgr.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
-            {
-                Instantiate(placeObject, hits[0].pose.position, hits[0].pose.rotation);
-            }
-        }
-    }
-    */
 }
