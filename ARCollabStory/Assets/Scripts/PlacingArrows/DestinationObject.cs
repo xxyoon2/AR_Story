a84951b2f6ce13a0eedb8b76ba0a1aded92f9380
Hyemin75 <wjsgpals0705@gmail.com>
2022-10-25 10:43:05 +0900

    [Create] Stage1 오브젝트와 목적지, 루트 오브젝트들 추가

--- ARCollabStory/Assets/Scripts/PlacingArrows/DestinationObject.cs
+++ ARCollabStory/Assets/Scripts/PlacingArrows/DestinationObject.cs
@@ -3,88 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-enum TypeOfDestinationObject
+public enum TypeOfDestinationObject
 {
     StartQuestDestination,
+    EndQuestDestination,
     StartMiniGameDestination,
-    EndQuestDestination
+    EndMiniGameDestination
 }
 
 public class DestinationObject : MonoBehaviour
 {
+    public TypeOfDestinationObject TypeOfDestination;
     public float Distance = 0.01f;
 
+    public GameObject Stage1Routes;
+    //스테이지 하나에 있는 루트들
+    private GameObject StartRoute;
+    private GameObject Secondroute;
+    private GameObject ThirdRoute;
+
     private GameObject _mainCamera;
-
-    private TypeOfDestinationObject _typeOfDestinationObject;
-    private bool _isQuestStart;
-    private bool _isQuestEnd;
-    private bool _isMiniGameStart;
-    public bool IsQuestStart
-    {
-        get
-        {
-            return _isQuestStart;
-        }
-        set
-        {
-            _isQuestStart = value;
-        }
-    }
-    public bool IsQuestEnd
-    {
-        get
-        {
-            return _isQuestEnd;
-        }
-        set
-        {
-            _isQuestEnd = value;
-        }
-    }
-
-    public bool IsMiniGameStart
-    {
-        get
-        {
-            return _isMiniGameStart;
-        }
-        set
-        {
-            _isMiniGameStart = value;
-        }
-    }
-
+  
     private void Awake()
     {
-        _isQuestStart = false;
-        _isMiniGameStart = false;
-        _isQuestEnd = false;
         _mainCamera = GameObject.Find("AR Camera");
+
+        StartRoute = Stage1Routes.GetComponent<Transform>().GetChild(0).gameObject;
+        Secondroute = Stage1Routes.GetComponent<Transform>().GetChild(1).gameObject;
+        ThirdRoute = Stage1Routes.GetComponent<Transform>().GetChild(2).gameObject;
     }
 
     private void Update()
     {
+
         double distanceToPlayer = Vector3.Distance(transform.position, _mainCamera.transform.position);
 
         //목적지와 플레이어와 거리가 가까워졌을 때 게임 상태 변형
         //"목적지에 도착했을 때"를 얻는 것은 이 코드에서 하면 됨
         if (distanceToPlayer < Distance)
         {
-            if (!_isMiniGameStart && _typeOfDestinationObject == TypeOfDestinationObject.StartMiniGameDestination)
-            {
-                _isMiniGameStart = true;
-            }
-            else if (!_isQuestStart && _typeOfDestinationObject == TypeOfDestinationObject.StartQuestDestination)
-            {
-                _isQuestStart = true;
-            }
-            else if (_typeOfDestinationObject == TypeOfDestinationObject.EndQuestDestination)
-            {
-                _isQuestEnd = true;
-            }
-
+            ActiveRoutes(TypeOfDestination);
         }
     }
 
+
+
+
+    public void ActiveRoutes(TypeOfDestinationObject typeOfDestinationObject)
+    {
+        switch (typeOfDestinationObject)
+        {
+            case TypeOfDestinationObject.StartQuestDestination:
+                if(Secondroute.activeSelf)
+                {
+                    Secondroute.SetActive(false);
+                }
+                StartRoute.SetActive(false);
+                break;
+            case TypeOfDestinationObject.EndQuestDestination:
+                Secondroute.SetActive(true);
+                break;
+            case TypeOfDestinationObject.StartMiniGameDestination:
+                Secondroute.SetActive(false);
+                break;
+        }
+    }
 }
