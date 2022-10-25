823ed43e7a446cb4de0a771896846c18e6085d3f
heeyoung8748 <103082973+heeyoung8748@users.noreply.github.com>
2022-10-25 11:13:35 +0900

    Update DirectionsInfo.cs

--- ARCollabStory/Assets/Scripts/UI/DirectionsInfo.cs
+++ ARCollabStory/Assets/Scripts/UI/DirectionsInfo.cs
@@ -23,11 +23,10 @@ public class DirectionsInfo : MonoBehaviour
         for(int i = 0; i < _areaCount; i++)
         {
             _directionAreas[i].OrderIndex = GameManager.Instance.LocationRecords[i + 2].DirectionIndex;
-            Debug.Log($"버튼연결해요{i},{GameManager.Instance.LocationRecords[i + 2].DirectionIndex}");
         }
 
         // 목적지의 상태에 따라 버튼 ui의 색상을 바꿔주는 함수 실행
-        Invoke("ButtonColorChange", 2f);
+        Invoke("ButtonColorChange");
     }
 
     void ButtonColorChange()
