74d6d6fb01c50b7d8d36c8179cb69d1345e00fc7
heeyoung8748 <103082973+heeyoung8748@users.noreply.github.com>
2022-10-26 13:19:41 +0900

    [Feat] 이야기책, 퍼즐 각각 파싱

--- ARCollabStory/Assets/Scripts/GameManager.cs
+++ ARCollabStory/Assets/Scripts/GameManager.cs
@@ -5,12 +5,12 @@ using CsvHelper.Configuration;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 public class GameManager : SingletonBehaviour<GameManager>
 {
     public UnityEvent DirectionsStatusUpdate = new UnityEvent();
     public UnityEvent SetCSVData = new UnityEvent();
     private List<LocationRecord> _locationRecords;
-
     // csv 파싱해서 저장할 리스트
     public List<LocationRecord> LocationRecords
     {
@@ -18,9 +18,22 @@ public class GameManager : SingletonBehaviour<GameManager>
         set { _locationRecords = value; }
     }
 
+    private (List<BookRecord>, List<PuzzleRecord>) _bookInfos;
+    public List<BookRecord> bookRecords
+    {
+        get { return _bookInfos.Item1; }
+        set { _bookInfos.Item1 = value; }
+    }
+    public List<PuzzleRecord> PuzzleRecords
+    {
+        get { return _bookInfos.Item2; }
+        set { _bookInfos.Item2 = value; }
+    }
+
     void Start()
     {
         _locationRecords = CSVParser.GetLocationInfos();
+        _bookInfos = CSVParser.GetBookInfos();
         SetCSVData.Invoke();
     }
 
@@ -28,5 +41,4 @@ public class GameManager : SingletonBehaviour<GameManager>
     {
         DirectionsStatusUpdate.Invoke();
     }
-
 }
