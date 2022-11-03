ba208ef6cb36d53604e698564a3bd112674b7ed6
Hyemin75 <wjsgpals0705@gmail.com>
2022-11-03 15:26:08 +0900

    [Feat] ArrowRocater 구현
    전에 만들었던 AR + GPS 화살표 삭제함

---
+++ ARCollabStory/Assets/Scripts/ArrowNev/ArrowRocater.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRocater : MonoBehaviour
+{
+    private Transform _destinationPos;
+
+    private void Awake()
+    {
+        // 나중에 CSV 양식에 따라 수정 가능성 있음
+        _destinationPos = GameManager.Instance.CurrentDestination.transform;
+    }
+
+    /// <summary>
+    /// Y축으로만 목적지를 향해 회전하는 함수
+    /// Update문에 넣어야 함
+    /// </summary>
+    private void UpdateArrowRotation()
+    {
+        //테스트용
+        transform.LookAt(new Vector3(_destinationPos.position.x, transform.position.y, _destinationPos.position.z));
+    }
+
+    /// <summary>
+    /// 화살표 네비게이션을 끄고 키는 함수
+    /// </summary>
+    /// <param name="On">true : 킴 , false : 끔</param>
+    public void SetActiveArrowRocater(bool isActive)
+    {
+       GetComponentInChildren<Transform>().gameObject.SetActive(isActive);
+    }
+
+}
