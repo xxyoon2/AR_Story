55d95582486e79f77d18e88ee357b1180390bfb2
Hyemin75 <wjsgpals0705@gmail.com>
2022-11-04 17:31:03 +0900

    [Feat] 충돌하면 사라지는 기억조각 구현

--- ARCollabStory/Assets/Scripts/MemoryFragment.cs
+++ ARCollabStory/Assets/Scripts/MemoryFragment.cs
@@ -10,12 +10,22 @@ public class MemoryFragment : MonoBehaviour
         get { return _num;}
         set { _num = value;}
     }
+    private bool _isStart = false;
+    private Vector3 _minScale = new Vector3(0.001f, 0.001f, 0.001f);
+    private float _speed;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "MainCamera")
         {
-            Debug.Log($"{Num}번째 {gameObject.name}과 닿았다.");
+            if(_isStart)
+            {
+                Debug.Log($"{Num}번째 {gameObject.name}과 닿았다.");
+                transform.GetChild(0).gameObject.SetActive(false);
+                Debug.Log($"기억 파편이 현재 {transform.GetChild(0).gameObject.activeSelf} 상태임");
+
+            }
+            _isStart = true;
         }
     }
 
